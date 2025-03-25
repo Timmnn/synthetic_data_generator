e9ba24f5033a7b6b58a14053bb0ddd414664c2de
Timm Nicolaizik <timmmmnn@gmail.com>
2025-03-25 16:07:50 +0100

    Modified equities generator.

--- src/generators/equities/mod.rs
+++ src/generators/equities/mod.rs
@@ -4,6 +4,11 @@ pub struct EquitiesGenerator {}
 
 impl Generator for EquitiesGenerator {
     fn generate(config: crate::config_reader::DatasetConfig) -> () {
-        todo!()
+        let mut time = config.daterange.from;
+
+        while (time < config.daterange.to) {
+            println!("Generating Entry...");
+            time = time.checked_add_days(chrono::Days::new(1)).unwrap();
+        }
     }
 }
